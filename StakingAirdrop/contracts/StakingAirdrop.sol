8a6b5d5430c826b527f735b6a3af6e811469dfff
Brandon Torres <44816281+btorressz@users.noreply.github.com>
2024-10-08 14:52:44 -0700

    Update StakingAirdrop.sol

--- StakingAirdrop/contracts/StakingAirdrop.sol
+++ StakingAirdrop/contracts/StakingAirdrop.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
-// Import OpenZeppelin contracts for safe ERC20 operations and security
+//  Import OpenZeppelin contracts 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
